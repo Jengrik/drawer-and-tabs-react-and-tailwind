795e13b15b62cd25d8ae75a2d7c9fd229416dcf1
Jhon Navarrete <JhonErik15769@hotmail.com>
2023-07-03 17:13:49 -0500

    Changing the project name in order to deploy it in GithubPages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-tailwind-basic-setup/",
+  base: "/drawer-and-tabs-react-and-tailwind/",
 });
