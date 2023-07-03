e0d73269058da1c3c2de41f085aae8c068237ac0
Jhon Navarrete <JhonErik15769@hotmail.com>
2023-07-03 17:10:11 -0500

    Basic Environment configuration for the project.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "tailwindcss/tailwind.css";
+// import ViteLogo from "./assets/images/ViteLogo";
+// import ReactLogo from "./assets/images/ReactLogo";
+// import TailwindLogo from "./assets/images/TailwindLogo";
+
+function App() {
+  return (
+    <>
+      <main className="w-[80%] max-w-lg p-5 flex flex-col justify-center items-center">
+        <div>Jhon Navarrete</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
